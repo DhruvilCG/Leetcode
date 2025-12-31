cd95cc27c98c7000fb574088c5e18de353b0839a
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2025-12-31 19:04:48 +0530

    Time: 0 ms (100%), Space: 9.3 MB (78.78%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,27 +4,48 @@ public:
         if (s.length() != t.length())
             return false;
 
-        unordered_map<char, char> m1, m2;
+        // unordered_map<char, char> m1, m2;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (m1.find(s[i]) != m1.end()) {
-                if (m1[s[i]] != t[i]) {
-                    return false;
-                }
-            } else {
-                m1[s[i]] = t[i];
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (m1.find(s[i]) != m1.end()) {
+        //         if (m1[s[i]] != t[i]) {
+        //             return false;
+        //         }
+        //     } else {
+        //         m1[s[i]] = t[i];
+        //     }
+
+        //     if (m2.find(t[i]) != m2.end()) {
+        //         if (m2[t[i]] != s[i]) {
+        //             return false;
+        //         }
+
+        //     } else {
+        //         m2[t[i]] = s[i];
+        //     }
+        // }
+
+        // return true;
+
+
+
+
+
+
+
+        int m1[256] = {0} ;
+        int m2[256] = {0} ;
+
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (m1[s[i]] != m2[t[i]]) {
+                return false ;
             }
 
-            if (m2.find(t[i]) != m2.end()) {
-                if (m2[t[i]] != s[i]) {
-                    return false;
-                }
-
-            } else {
-                m2[t[i]] = s[i];
-            }
+            m1[s[i]] = i+1 ;
+            m2[t[i]] = i+1 ;
         }
 
-        return true;
+        return true ;
+
     }
 };
