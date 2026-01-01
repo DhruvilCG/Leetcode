3333d6eadd58dc8dccc8bd1140d262c7461fc0bb
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-01 17:22:30 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -15,25 +15,83 @@ public:
 
 
 
-        for (int i = 0 ; i < matrix.size() ; i++) {
-            if (target > matrix[i][matrix[i].size()-1]) {
-                continue ;
-            }
-            int st = 0 ;
-            int end = matrix[i].size()-1 ;
-            while (st <= end) {
-                int mid = st + (end - st) / 2 ;
+        // for (int i = 0 ; i < matrix.size() ; i++) {
+        //     int st = 0 ;
+        //     int end = matrix[i].size()-1 ;
+        //     while (st <= end) {
+        //         int mid = st + (end - st) / 2 ;
 
-                if (matrix[i][mid] == target) {
-                    return true ;
-                } else if (matrix[i][mid] > target) {
-                    end = mid-1 ;
-                } else {
-                    st = mid+1 ;
+        //         if (matrix[i][mid] == target) {
+        //             return true ;
+        //         } else if (matrix[i][mid] > target) {
+        //             end = mid-1 ;
+        //         } else {
+        //             st = mid+1 ;
+        //         }
+        //     }
+        // }
+
+        // return false ;
+
+
+
+
+
+
+
+        
+        // for (int i = 0 ; i < matrix.size() ; i++) {
+        //     if (target > matrix[i][matrix[i].size()-1]) {
+        //         continue ;
+        //     }
+        //     int st = 0 ;
+        //     int end = matrix[i].size()-1 ;
+        //     while (st <= end) {
+        //         int mid = st + (end - st) / 2 ;
+
+        //         if (matrix[i][mid] == target) {
+        //             return true ;
+        //         } else if (matrix[i][mid] > target) {
+        //             end = mid-1 ;
+        //         } else {
+        //             st = mid+1 ;
+        //         }
+        //     }
+        // }
+
+        // return false ;
+
+
+
+
+
+
+
+        int stRow = 0, endRow = matrix.size() - 1;
+        while (stRow <= endRow) {
+            int midRow = stRow + (endRow - stRow) / 2;
+
+            if (matrix[midRow][0] <= target &&
+                matrix[midRow][matrix[midRow].size() - 1] >= target) {
+                int st = 0, end = matrix[midRow].size() - 1;
+                while (st <= end) {
+                    int mid = st + (end - st) / 2;
+                    if (matrix[midRow][mid] == target) {
+                        return true;
+                    } else if (matrix[midRow][mid] < target) {
+                        st = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
                 }
+                return false ;
+            } else if (matrix[midRow][0] > target) {
+                endRow = midRow-1 ;
+            } else {
+                stRow = midRow + 1 ;
             }
         }
 
-        return false ;
+        return false;
     }
 };
