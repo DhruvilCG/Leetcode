91b791b4057706969f99f6ecc3cb568a78033501
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-02 11:06:50 +0530

    Time: 15 ms (32.66%), Space: 34 MB (26.32%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map <int,int> m1 ;
+        for (int i = 0 ; i < nums.size() ; i++) {
+            m1[nums[i]]++ ;
+            if (m1[nums[i]] == (nums.size()/2)) {
+                return nums[i] ;
+            }
+        }
+
+        return -1 ;
+
+
+        // unordered_set<int> s1 ;
+        // for (int i = 0 ; i < nums.size() ; i++) {
+        //     if (s1.find(nums[i]) != s1.end()) {
+        //         return nums[i] ;
+        //     } else {
+        //         s1.insert(nums[i]) ;
+        //     }
+        // }
+        // return -1 ;
+    }
+};
