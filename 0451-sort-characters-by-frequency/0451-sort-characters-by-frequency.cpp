3db88a88d9d780b89b7f5871e459bd7aa34dc708
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-02 15:01:21 +0530

    Time: 10 ms (8.97%), Space: 19.3 MB (5.23%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,52 @@
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char, int> m1;
+//         for (int i = 0; i < s.length(); i++) {
+//             m1[s[i]]++;
+//         }
+
+//         vector<pair<char, int>> v(m1.begin(), m1.end());
+//         sort(v.begin(), v.end(),
+//              [](auto& a, auto& b) { return a.second < b.second; });
+
+//         string ans = "";
+//         for (int i = v.size() - 1; i >= 0; i--) {
+//             for (int j = 0; j < v[i].second; j++) {
+//                 ans += v[i].first;
+//             }
+//         }
+
+//         return ans ;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> m1;
-        for (int i = 0; i < s.length(); i++) {
-            m1[s[i]]++;
+
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
         }
 
-        vector<pair<char, int>> v(m1.begin(), m1.end());
-        sort(v.begin(), v.end(),
-             [](auto& a, auto& b) { return a.second < b.second; });
+        vector<string> bucket(s.size() + 1);
+
+        for (auto &p : freq) {
+            bucket[p.second] += string(p.second, p.first);
+        }
 
         string ans = "";
-        for (int i = v.size() - 1; i >= 0; i--) {
-            for (int j = 0; j < v[i].second; j++) {
-                ans += v[i].first;
-            }
+        for (int i = bucket.size() - 1; i >= 0; i--) {
+            ans += bucket[i];
         }
 
-        return ans ;
+        return ans;
     }
 };
