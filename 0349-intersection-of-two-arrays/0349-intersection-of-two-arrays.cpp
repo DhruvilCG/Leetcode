0af3b0c6a3dcb93bf1876bce149263a43b9b8dca
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-03 23:34:01 +0530

    Time: 0 ms (100%), Space: 14.6 MB (16.35%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // vector<int> ans = {} ;
+        // unordered_map<int , int> m1 ;
+
+        // for (int i = 0 ; i < nums1.size() ; i++) {
+        //     m1[nums1[i]]++ ;
+        // }
+
+        // for (int i = 0 ; i < nums2.size() ; i++) {
+        //     if (m1.find(nums2[i]) != m1.end() && m1[nums2[i]] != 0) {
+        //         ans.push_back(nums2[i]) ;
+        //         m1[nums2[i]] = 0 ;
+        //     }
+        // }
+
+        // return ans ;
+
+
         vector<int> ans = {} ;
-        unordered_map<int , int> m1 ;
+        unordered_set<int> s1 ;
+        unordered_set<int> s2 ;
 
         for (int i = 0 ; i < nums1.size() ; i++) {
-            m1[nums1[i]]++ ;
+            s1.insert(nums1[i]) ;
+        }
+        for (int i = 0 ; i < nums2.size() ; i++) {
+            s2.insert(nums2[i]) ;
         }
 
-        for (int i = 0 ; i < nums2.size() ; i++) {
-            if (m1.find(nums2[i]) != m1.end() && m1[nums2[i]] != 0) {
-                ans.push_back(nums2[i]) ;
-                m1[nums2[i]] = 0 ;
+        for (int num: s1) {
+            if (s2.find(num) != s2.end()) {
+                ans.push_back(num) ;
             }
         }
 
