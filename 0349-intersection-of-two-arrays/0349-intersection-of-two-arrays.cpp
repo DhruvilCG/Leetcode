6b7dde6bee2734aa09057a7b87ba068034f94ac3
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-03 23:40:24 +0530

    Time: 0 ms (100%), Space: 14.4 MB (48.12%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -18,20 +18,40 @@ public:
         // return ans ;
 
 
+        // vector<int> ans = {} ;
+        // unordered_set<int> s1 ;
+        // unordered_set<int> s2 ;
+
+        // for (int i = 0 ; i < nums1.size() ; i++) {
+        //     s1.insert(nums1[i]) ;
+        // }
+        // for (int i = 0 ; i < nums2.size() ; i++) {
+        //     s2.insert(nums2[i]) ;
+        // }
+
+        // for (int num: s1) {
+        //     if (s2.find(num) != s2.end()) {
+        //         ans.push_back(num) ;
+        //     }
+        // }
+
+        // return ans ;
+
+
+
+
+        
         vector<int> ans = {} ;
-        unordered_set<int> s1 ;
-        unordered_set<int> s2 ;
+        unordered_set<int> s ;
 
         for (int i = 0 ; i < nums1.size() ; i++) {
-            s1.insert(nums1[i]) ;
-        }
-        for (int i = 0 ; i < nums2.size() ; i++) {
-            s2.insert(nums2[i]) ;
+            s.insert(nums1[i]) ;
         }
 
-        for (int num: s1) {
-            if (s2.find(num) != s2.end()) {
-                ans.push_back(num) ;
+        for (int i = 0 ; i < nums2.size() ; i++) {
+            if (s.find(nums2[i]) != s.end()) {
+                ans.push_back(nums2[i]) ;
+                s.erase(nums2[i]) ;
             }
         }
 
