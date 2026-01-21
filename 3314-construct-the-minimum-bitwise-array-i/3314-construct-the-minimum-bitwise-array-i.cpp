ac94e5f0fc4e00c865829d2bed06ad3dd18e74fd
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-21 23:51:40 +0530

    Time: 0 ms (100%), Space: 25.2 MB (25.71%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> ans(nums.size()) ;
+        // vector<int> ans(nums.size()) ;
+
+        // for (int i = 0 ; i < nums.size() ; i++) {
+        //     for (int j = 0 ; j < nums[i] ; j++) {
+        //         if ((j|(j+1)) == nums[i]) {
+        //             ans[i] = j ;
+        //             break;
+        //         } else {
+        //             ans[i] = -1;
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+
+        vector<int> ans ;
 
         for (int i = 0 ; i < nums.size() ; i++) {
-            for (int j = 0 ; j < nums[i] ; j++) {
-                if ((j|(j+1)) == nums[i]) {
-                    ans[i] = j ;
-                    break;
-                } else {
-                    ans[i] = -1;
+            if (nums[i] == 2) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            bool found = false;
+            for (int j = 1 ; j < 32 ; j++) {
+                if ((nums[i] & (1 << j)) > 0) {
+                    continue;
                 }
+
+                int x = (nums[i] ^ (1 << (j-1))) ;
+                found = true;
+                ans.push_back(x);
+                break;
+            }
+
+            if (found == false) {
+                ans.push_back(-1) ;
             }
         }
 
