6afb84de5d9ba0e79a30f8929aceebacbbd4156a
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-22 16:31:20 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.22%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         s.erase(s.find_last_not_of(' ')+1);
+        if (s.size() == 0) {
+            return 0;
+        }
+
         int idx = -1 ;
         for (int i = s.size()-1 ; i >= 0 ; i--) {
-            if (s[i] == 32) {
+            if (s[i] == ' ') {
                 idx = i;
                 break;
             }
