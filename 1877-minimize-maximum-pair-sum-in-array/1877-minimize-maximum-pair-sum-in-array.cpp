5ef412af7df3549121c7f91cbeded5568b58e1d8
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-24 23:46:46 +0530

    Time: 166 ms (76.78%), Space: 99.9 MB (64.82%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int maxSum1 = INT_MIN;
-        int maxSum2 = nums[0]+nums[nums.size()-1];
-        int maxSum3 = INT_MIN;
+        // sort(nums.begin() , nums.end());
+        // int maxSum1 = INT_MIN;
+        // int maxSum2 = nums[0]+nums[nums.size()-1];
+        // int maxSum3 = INT_MIN;
 
-        for (int i = 0 ; i < nums.size()-1 ; i=i+2) {
-            maxSum1 = max(nums[i]+nums[i+1] , maxSum1);
-        }
+        // for (int i = 0 ; i < nums.size()-1 ; i=i+2) {
+        //     maxSum1 = max(nums[i]+nums[i+1] , maxSum1);
+        // }
         
-        for (int i = 1 ; i < nums.size()-1 ; i=i+2) {
-            maxSum2 = max(nums[i]+nums[i+1] , maxSum2);
-        }
+        // for (int i = 1 ; i < nums.size()-1 ; i=i+2) {
+        //     maxSum2 = max(nums[i]+nums[i+1] , maxSum2);
+        // }
+        
+        // for (int i = 0 ; i < nums.size()/2 ; i++) {
+        //     maxSum3 = max(nums[i]+nums[nums.size()-i-1] , maxSum3);
+        // }
+
+        // int min1 = min(maxSum1 , maxSum2);
+        // return min(min1 , maxSum3);
+
+
+
+        sort(nums.begin() , nums.end());
+        int ans = INT_MIN;
         
         for (int i = 0 ; i < nums.size()/2 ; i++) {
-            maxSum3 = max(nums[i]+nums[nums.size()-i-1] , maxSum3);
+            ans = max(nums[i]+nums[nums.size()-i-1] , ans);
         }
 
-        int min1 = min(maxSum1 , maxSum2);
-        return min(min1 , maxSum3);
+        return ans;
     }
 };
