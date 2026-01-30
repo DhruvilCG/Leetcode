7d0fda98b7eadb05fca67ac38bd20161036f338f
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-01-30 10:26:23 +0530

    Time: 2 ms (53.87%), Space: 7.7 MB (48.27%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,15 +1,52 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        string ans = "" ;
-        for (int i = 0 ; i < address.size() ; i++) {
-            if (address[i] == '.') {
-                ans += "[.]";
-            } else {
-                ans += address[i];
-            }
+
+        // Space = O(n)    Time = O(n)
+
+        // string ans = "" ;
+        // for (int i = 0 ; i < address.size() ; i++) {
+        //     if (address[i] == '.') {
+        //         ans += "[.]";
+        //     } else {
+        //         ans += address[i];
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+
+
+        // Space = O(1)    Time = O(n)
+
+        int dots = 0 ;
+        int oldSize = address.size();
+        for (int i : address) {
+            if (i == '.') {
+                dots++;
+            } 
         }
 
-        return ans;
+        address.resize(oldSize + dots*2);
+
+        int i = oldSize-1;
+        int j = address.size()-1;
+
+        while (i >= 0) {
+            if (address[i] == '.') {
+                address[j--] = ']';
+                address[j--] = '.';
+                address[j--] = '[';
+            } else {
+                address[j--] = address[i];
+            } 
+
+            i--;
+        }
+
+        return address;
     }
 };
