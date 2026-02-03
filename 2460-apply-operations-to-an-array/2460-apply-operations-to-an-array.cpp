0b9b2d6aaeb90c4e9915a7e7785cc2da804b89cf
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-03 22:47:10 +0530

    Time: 0 ms (100%), Space: 12.9 MB (99.37%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+
+        return nums;
+    }
+};
