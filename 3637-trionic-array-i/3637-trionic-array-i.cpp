75a394c49f72db47ae406beace5a42c175660b79
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-03 22:22:38 +0530

    Time: 0 ms (100%), Space: 27.8 MB (35.38%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -1,40 +1,70 @@
+// class Solution {
+// public:
+//     bool isTrionic(vector<int>& nums) {
+//         int p = 0 , q = 0;
+//         for (int i = 0 ; i < nums.size()-1 ; i++) {
+//             if (nums[i] == nums[i+1]) {
+//                 return false;
+//             }
+//             if (nums[i] > nums[i+1]) {
+//                 p = i;
+//                 break;
+//             }
+//         }
+//         if (p == nums.size()-1 || p == 0) {
+//             return false;
+//         }
+//         for (int i = p ; i < nums.size()-1 ; i++) {
+//             if (nums[i] == nums[i+1]) {
+//                 return false;
+//             }
+//             if (nums[i] < nums[i+1]) {
+//                 q = i;
+//                 break;
+//             }
+//         }
+//         for (int i = q ; i < nums.size()-1 ; i++) {
+//             if (nums[i] == nums[i+1]) {
+//                 return false;
+//             }
+//             if (nums[i] > nums[i+1]) {
+//                 return false;
+//             }
+//         }
+
+//         if (q == nums.size()-1) {
+//             return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
-        int p = 0 , q = 0;
+        int p = -1 , q = -1;
         for (int i = 0 ; i < nums.size()-1 ; i++) {
             if (nums[i] == nums[i+1]) {
                 return false;
             }
-            if (nums[i] > nums[i+1]) {
+            if (nums[i] > nums[i+1] && p == -1) {
                 p = i;
-                break;
             }
-        }
-        if (p == nums.size()-1 || p == 0) {
-            return false;
-        }
-        for (int i = p ; i < nums.size()-1 ; i++) {
-            if (nums[i] == nums[i+1]) {
-                return false;
-            }
-            if (nums[i] < nums[i+1]) {
+            if (nums[i] < nums[i+1] && q == -1 && p != -1) {
                 q = i;
-                break;
             }
-        }
-        for (int i = q ; i < nums.size()-1 ; i++) {
-            if (nums[i] == nums[i+1]) {
-                return false;
-            }
-            if (nums[i] > nums[i+1]) {
-                return false;
-            }
-        }
 
-        if (q == nums.size()-1) {
-            return false;
-        }
-        return true;
+            if (q != -1 && nums[i] > nums[i+1]) {
+                return false;
+            }
+        }   
+
+        if (p == 0) return false;
+        return (p != -1 && q != -1);
     }
 };
