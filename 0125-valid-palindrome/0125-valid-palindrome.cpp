5be7ea8a81d78daf7d93c2ecc7d6fb0b059447e7
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-05 12:08:20 +0530

    Time: 0 ms (100%), Space: 10 MB (54.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string test = "";
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != 0 && ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122) || (s[i] >= 48 && s[i] <= 57))) {
-                test += tolower(s[i]);
-            }
-        }
+        // string test = "";
+        // for (int i = 0; i < s.size(); i++) {
+        //     if (s[i] != 0 && ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122) || (s[i] >= 48 && s[i] <= 57))) {
+        //         test += tolower(s[i]);
+        //     }
+        // }
 
-        for (int i = 0 ; i < test.size()/2 ; i++) {
-            if (test[i] != test[test.size()-1-i]) {
+        // for (int i = 0 ; i < test.size()/2 ; i++) {
+        //     if (test[i] != test[test.size()-1-i]) {
+        //         return false;
+        //     }
+        // }
+        // return true ;
+        
+        
+        
+        
+
+        int st = 0;
+        int end = s.size() - 1;
+
+        while (st < end) {
+
+            while (st < end &&
+                  !((s[st] >= 'A' && s[st] <= 'Z') ||
+                    (s[st] >= 'a' && s[st] <= 'z') ||
+                    (s[st] >= '0' && s[st] <= '9'))) {
+                st++;
+            }
+
+            while (st < end &&
+                  !((s[end] >= 'A' && s[end] <= 'Z') ||
+                    (s[end] >= 'a' && s[end] <= 'z') ||
+                    (s[end] >= '0' && s[end] <= '9'))) {
+                end--;
+            }
+
+            if (tolower(s[st]) != tolower(s[end])) {
                 return false;
             }
+
+            st++;
+            end--;
         }
-        return true ;
+
+        return true;
+
     }
 };
