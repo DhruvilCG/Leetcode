f37d14f72c3b2f8f0f853307021aee9ec0d413f2
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-20 10:42:01 +0530

    Time: 3 ms (59.11%), Space: 8.9 MB (72.85%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        // if (n == 1) {
+        //     return true;
+        // }
+
+        // long long prev = 1;
+        // for (int i = 3 ; i <= n ; i=i+3) {
+        //     prev *= 3;
+        //     if (prev == n || prev >= n) {
+        //         break;
+        //     }
+        // }
+
+        // if (prev == n) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+
+
+
+
+        
+        if (n <= 0) {
+            return false;
+        }
         if (n == 1) {
             return true;
         }
 
         long long prev = 1;
-        for (int i = 3 ; i <= n ; i=i+3) {
+        while (prev < n) {
             prev *= 3;
-            if (prev == n || prev >= n) {
-                break;
-            }
         }
 
         if (prev == n) {
