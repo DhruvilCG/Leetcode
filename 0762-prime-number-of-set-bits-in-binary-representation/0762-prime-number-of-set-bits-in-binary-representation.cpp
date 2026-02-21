2e92fcb269ec62e68bd5c5c08c02ee9d7370c8b7
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-22 00:50:44 +0530

    Time: 4 ms (55.49%), Space: 8.4 MB (13.87%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        int ans = 0;
-        for (int i = left; i <= right; i++) {
+        // int ans = 0;
+        // for (int i = left ; i <= right ; i++) {
+        //     int count = __builtin_popcount(i);
+        //     bool flag = false;
+        //     if (count == 0 || count == 1) {
+        //         continue;
+        //     }
+        //     if (count == 2) {
+        //         ans++;
+        //         continue;
+        //     }
+        //     for (int j = 2 ; j < count ; j++) {
+        //         if (count%j == 0) {
+        //             flag = true ;
+        //             break;
+        //         }
+        //     }
+        //     if (!flag) {
+        //         ans++; 
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+
+
+
+        unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+        int ans = 0 ;
+        for (int i = left ; i <= right ; i++) {
             int count = __builtin_popcount(i);
-            bool flag = false;
-            if (count == 0 || count == 1) {
-                continue;
-            }
-            if (count == 2) {
-                ans++;
-                continue;
-            }
-            for (int j = 2; j < count; j++) {
-                if (count % j == 0) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
+            if (primes.find(count) != primes.end()) {
                 ans++;
             }
         }
 
         return ans;
+
     }
 };
