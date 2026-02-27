84bcb4c2967f10895e450e885dbcf6ef100d4605
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-02-27 15:11:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (50.73%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int minOperations(int n) {
+        // int st = 1 , end = (2*(n-1) + 1);
+        // int count = 0 ;
+
+        // while (st != end){
+        //     count++;
+        //     end--;
+        //     st++;
+        // }
+
+        // for (int i = 1 ; i < n/2 ; i++) {
+        //     count += (st - (2*i+1));
+        // }
+        // return count;
+
+
+
+        int count = 0;
         int st = 1 , end = (2*(n-1) + 1);
-        int count = 0 ;
-
-        while (st != end){
-            count++;
-            end--;
-            st++;
-        }
-
+        st = end = (st+end)/2;
+        count += st-1;
+        
         for (int i = 1 ; i < n/2 ; i++) {
             count += (st - (2*i+1));
         }
+
         return count;
     }
 };
