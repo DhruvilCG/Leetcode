080bed643b0beb4ad23574fae59518302fb9ae44
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-03-01 18:44:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.02%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        int endIdx;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u') {
+                continue;
+            } else {
+                endIdx = i;
+                break;
+            }
+        }
+
+        return s.substr(0, endIdx + 1);
+    }
+};
