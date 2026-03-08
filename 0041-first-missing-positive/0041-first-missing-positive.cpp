fce920a6aedfcbd5e149e4a78fb7719363057bf7
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-03-08 17:00:59 +0530

    Time: 1 ms (60.05%), Space: 55.1 MB (62.4%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s;
-        for (int i = 0; i < nums.size(); i++) {
-            s.insert(nums[i]);
-        }
+        // unordered_set<int> s;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     s.insert(nums[i]);
+        // }
 
-        for (int i = 1; i <= nums.size() + 1; i++) {
-            if (s.find(i) == s.end()) {
-                return i;
+        // for (int i = 1; i <= nums.size(); i++) {
+        //     if (s.find(i) == s.end()) {
+        //         return i;
+        //     }
+        // }
+
+        // return nums.size()+1;
+
+
+
+        for (int i = 0 ; i < nums.size() ; i++) {
+            while (true) {
+                if (nums[i] <= 0 || nums[i] > nums.size()) {
+                    break;
+                }
+                int val = nums[i];
+                if (val == i+1) {
+                    break;
+                }
+                int correctIdx = val-1;
+                if (nums[correctIdx] == val) {
+                    break;
+                }
+
+                swap(nums[i] , nums[correctIdx]);
             }
         }
 
-        return nums.size();
+        for (int i = 0 ; i < nums.size() ; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+
+        return nums.size()+1;
     }
 };
