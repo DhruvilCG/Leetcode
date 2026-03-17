c2c0de0dc2aaca2e11ae4138321631f587910c12
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-03-17 10:17:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (78.43%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> vec;
-        for (int i = 1; i <= n; i++) {
-            vec.push_back(i);
+        // vector<int> vec;
+        // for (int i = 1; i <= n; i++) {
+        //     vec.push_back(i);
+        // }
+
+        // int idx = 0;
+        // while (vec.size() > 1) {
+        //     idx = (idx + k - 1) % vec.size();
+        //     vec.erase(vec.begin() + idx);
+        // }
+
+        // return vec[0];
+
+
+
+        int winner = 0;
+        for (int i = 1 ; i <= n ; i++) {
+            winner = (winner+k)%i;
         }
 
-        int idx = 0;
-        while (vec.size() > 1) {
-            idx = (idx + k - 1) % vec.size();
-            vec.erase(vec.begin() + idx);
-        }
-
-        return vec[0];
+        return winner+1;
     }
 };
