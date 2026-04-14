4151bb713a15673ac446895946467316842cb6cd
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-04-14 18:16:45 +0530

    Time: 151 ms (79.34%), Space: 116.8 MB (68.39%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPrime(int num) {
+        if (num <= 1)
+            return false;
+        if (num == 2)
+            return true;
+        if (num % 2 == 0)
+            return false;
+
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int minOperations(vector<int>& nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = nums[i];
+
+            if (i % 2 == 0) {
+                while (!isPrime(temp)) {
+                    temp++;
+                    count++;
+                }
+            } else {
+                while (isPrime(temp)) {
+                    temp++;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
