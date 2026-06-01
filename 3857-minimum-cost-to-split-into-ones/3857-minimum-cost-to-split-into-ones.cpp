96bd5a7aa7961ed3269daab74a0872882ba7e971
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-06-01 22:21:11 +0530

    Time: 1 ms (28.48%), Space: 8.3 MB (36.19%) - LeetHub

---
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCost(int n) {
+        int a = n - 1;
+        int cost = a;
+        while (a > 1) {
+            a -= 1;
+            cost += a;
+        }
+
+        return cost;
+    }
+};
