8e57e22b42ee6b50f129866974dcbbb2dad1894a
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-06-13 01:50:38 +0530

    Time: 8 ms (23.38%), Space: 9.2 MB (35.15%) - LeetHub

--- 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
@@ -17,7 +17,7 @@ public:
                 continue;
             }
             int count = 0;
-            for (int j = 2; j <= i / 2; j++) {
+            for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     count = 1;
                     break;
