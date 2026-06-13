ea50e94400c6db528b57951a503e7e56110ab3c0
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2026-06-13 15:10:46 +0530

    Time: 4 ms (15.86%), Space: 43.5 MB (77.6%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        vector<char> letters = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r',
-                                'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i',
-                                'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+        // vector<char> letters = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r',
+        //                         'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i',
+        //                         'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+        // string ans = "";
+        // for (int i = 0; i < words.size(); i++) {
+        //     int sum = 0;
+        //     for (int j = 0; j < words[i].size(); j++) {
+        //         sum += weights[words[i][j] - 'a'];
+        //     }
+        //     sum %= 26;
+        //     ans += letters[sum];
+        // }
+
+        // return ans;
+
+
+
+
+
+
+
+
+        
         string ans = "";
         for (int i = 0; i < words.size(); i++) {
             int sum = 0;
             for (int j = 0; j < words[i].size(); j++) {
                 sum += weights[words[i][j] - 'a'];
             }
-            sum %= 26;
-            ans += letters[sum];
+            ans += char('z' - (sum%26));
         }
 
         return ans;
